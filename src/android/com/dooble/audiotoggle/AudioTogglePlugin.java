2a97147442c1d8c7637054e0be1cfb6f9ab0daee
p.mikolajczak@tkhtechnology.com <p.mikolajczak@tkhtechnology.com>
2022-10-25 21:07:17 +0200

    fix: MX002-7256 change not supported audio mode for Android 12

--- src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
+++ src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
@@ -10,46 +10,46 @@ import android.media.AudioManager;
 
 public class AudioTogglePlugin extends CordovaPlugin {
 	public static final String ACTION_SET_AUDIO_MODE = "setAudioMode";
-	
+
 	@Override
-	public boolean execute(String action, JSONArray args, 
-			CallbackContext callbackContext) throws JSONException {	
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
 		if (action.equals(ACTION_SET_AUDIO_MODE)) {
 			if (!setAudioMode(args.getString(0))) {
 				callbackContext.error("Invalid audio mode");
 				return false;
 			}
-			
+
 			return true;
 		}
-		
+
 		callbackContext.error("Invalid action");
 		return false;
 	}
-	
+
 	public boolean setAudioMode(String mode) {
 	    Context context = webView.getContext();
-	    AudioManager audioManager = 
+	    AudioManager audioManager =
 	    	(AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-	    
+
 	    if (mode.equals("earpiece")) {
 	    	audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
 	    	audioManager.setSpeakerphoneOn(false);
 	        return true;
-	    } else if (mode.equals("speaker")) {        
-	    	audioManager.setMode(AudioManager.STREAM_MUSIC);
+	    } else if (mode.equals("speaker")) {
+	    	audioManager.setMode(AudioManager.MODE_NORMAL);
 	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
-	    } else if (mode.equals("ringtone")) {        
+	    } else if (mode.equals("ringtone")) {
 	    	audioManager.setMode(AudioManager.MODE_RINGTONE);
 	    	audioManager.setSpeakerphoneOn(false);
-	        return true; 
-	    } else if (mode.equals("normal")) {        
+	        return true;
+	    } else if (mode.equals("normal")) {
 	    	audioManager.setMode(AudioManager.MODE_NORMAL);
 	    	audioManager.setSpeakerphoneOn(false);
 	        return true;
 	    }
-	    
+
 	    return false;
 	}
 
